71243e03b5e6def8dc3eaabcb7be519d97e682c3
Amha-N <91714632+Amha-N@users.noreply.github.com>
2023-06-14 22:40:30 -0700

    Q1

---
+++ class.js
@@ -0,0 +1,24 @@
+console.log("class")
+/*Working with variables
+importance: 2
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”)*/
+
+let admin, name ;   //declare two variable at once
+ name  = "John" ;        
+ admin = name; // copy "Jone" from name into admin
+ //now two variables hold the sam value
+alert(admin); //"John"
+console.log(admin)
+
+
+
+
+
+
+
+
+
+
