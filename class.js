e739caaaacee263c5fb866239e6c7415dc3980bf
Amha-N <91714632+Amha-N@users.noreply.github.com>
2023-06-15 18:26:19 -0700

    Boolean

--- class.js
+++ class.js
@@ -6,11 +6,18 @@ Assign the value "John" to name.
 Copy the value from name to admin.
 Show the value of admin using alert (must output “John”)*/
 
-let admin, name ;   //declare two variable at once
- name  = "John" ;        
- admin = name; // copy "Jone" from name into admin
- //now two variables hold the sam value
-console.log(admin);
+// let admin
+// let  name   //declare two variable at once
+//  name  = "John" ;        
+//  admin = name; // copy "Jone" from name into admin
+//  //now two variables hold the sam value
+// console.log(admin);
+let x = 123
+let y = 1
+
+console.log(Boolean(x))
+console.log(Boolean(y))
+
 
 
 
