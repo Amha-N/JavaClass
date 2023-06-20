ff53292b9bc2a2053b191bbcb18332033af3ee29
Amha-N <91714632+Amha-N@users.noreply.github.com>
2023-06-19 17:21:05 -0700

    T3

--- class.js
+++ class.js
@@ -1,6 +1,25 @@
-let name = "Ilya";
+/*let name = "Ilya";
 console.log(name);
 
 console.log( `hello ${1}` ); // ?
 console.log( `hello ${"name"}` ); // ?
 console.log( `hello ${name}` ); // ?
+*/
+/*
+let x = "class ";
+let Y = "now";
+console.log(x + Y);
+console.log('1' + 2);
+*/
+console.log(typeof (undefined));
+console.log(typeof (0));
+console.log(typeof (10n));
+console.log(typeof (true));
+console.log(typeof ("foo"));
+console.log(typeof (Symbol("id")));
+console.log(typeof (Math));
+console.log(typeof (null));
+console.log(typeof (alert));
+
+
+
